2c8749cd0de01f70ec515f72db5169b269f56c1c
LeveauArnaud <a.leveau@students.ephec.be>
2020-04-28 12:51:42 +0200

    update client

--- assets/js/pages/ClientInfosPage.jsx
+++ assets/js/pages/ClientInfosPage.jsx
@@ -1,9 +1,11 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Field from "../components/forms/Field";
 import { Link } from "react-router-dom";
 import axios from "axios";
 
 const ClientInfosPage = (props) => {
+
+    const { id = "new"} = props.match.params;
 
     const [client, setClient] = useState({
         nCli:0,
@@ -16,7 +18,7 @@ const ClientInfosPage = (props) => {
         codePostale:"",
         pays:"",
         email:"",
-        phone:0,
+        phone:"",
         photo:"https://via.placeholder.com/150",
         corrections:[],
         commandeMontures: [],
@@ -26,22 +28,79 @@ const ClientInfosPage = (props) => {
 
     });
     const [errors, setErrors] = useState({
-        lastname:"",
-        firstname:"",
+        nCli:"",
+        nom:"",
+        prenom:"",
         sexe:"",
-        birth:"",
-        street:"",
-        city:"",
-        country:"",
+        dateNaissance:"",
+        rue:"",
+        ville:"",
+        codePostale:"",
+        pays:"",
         email:"",
-        phone:""
+        phone:"",
+        photo:"",
 
     });
+
+    const [editing, setEditing ] = useState(false);
+
+    const fetcgClient = async id =>{
+        try{
+            //awit permet de attendre afin de ne récuperer que les data
+            const data = await axios
+                .get("https://127.0.0.1:8000/api/clients/"+id)
+                .then(response => response.data);
+            const {
+                nCli,
+                nom,
+                prenom,
+                sexe,
+                dateNaissance,
+                rue,
+                ville,
+                codePostale,
+                pays,
+                email,
+                phone,
+                photo
+            } = data;
+
+            setClient({
+                nCli,
+                nom,
+                prenom,
+                sexe,
+                dateNaissance,
+                rue,
+                ville,
+                codePostale,
+                pays,
+                email,
+                phone,
+                photo
+            });
+        }catch (e) {
+            console.log(e.response);
+        }
+
+    }
+
+
+    useEffect(() =>{
+        if(id !== "new") {
+            setEditing(true);
+            fetcgClient(id);
+
+        };
+    }, [id]);
+
+
 
     const handleChange = ({currentTarget}) =>{
         const { name, value} = currentTarget;
         if(name ==="codePostale" || name ==="phone" ){
-            setClient({...client, [name]: parseInt(value)});
+            setClient({...client, [name]: +value});
         }else {
             setClient({...client, [name]: value});
         }
@@ -52,22 +111,38 @@ const ClientInfosPage = (props) => {
     const handleSubmit = async event =>{
         //eviter de recharger la page
         event.preventDefault();
-        console.log(client.codePostale);
         try{
 
-            const response = await axios.post("https://localhost:8000/api/clients", client);
-            console.log(response.data);
+            if(editing){
+                const response = await axios.put("https://127.0.0.1:8000/api/clients/"+id, client);
+
+            }else {
+                const response = await axios.post("https://localhost:8000/api/clients", client);
+
+                props.history.replace("/clients");
+            }
+
+            setErrors({});
+
         }catch (error) {
-            console.log(error.response);
+
+            if(error.response.data.violations){
+                const apiErrors = {};
+                error.response.data.violations.forEach( violation =>{
+                    apiErrors[violation.propertyPath] = violation.message;
+                });
+                console.log(apiErrors);
+                setErrors(apiErrors);
+            }
         }
 
-        console.log(client);
     };
 
     return(
         <>
             <div className="jumbotron">
-            <h1>Création d'un client</h1>
+
+                {!editing && <h1>Création d'un client</h1> || <h1>Modification du client</h1>}
 
             <form onSubmit={handleSubmit}>
                 <div className="row">
@@ -78,7 +153,7 @@ const ClientInfosPage = (props) => {
                             placeHolder="Nom de famille du client"
                             value={client.nom}
                             onChange={handleChange}
-                            error={errors.lastname}
+                            error={errors.nom}
                         />
                         <Field
                             label="Prénom du client"
@@ -86,7 +161,7 @@ const ClientInfosPage = (props) => {
                             placeHolder="Prénom du client"
                             value={client.prenom}
                             onChange={handleChange}
-                            error={errors.firstname}
+                            error={errors.prenom}
                         />
                         <Field
                             label="Sexe"
@@ -103,7 +178,7 @@ const ClientInfosPage = (props) => {
                             value={client.dateNaissance}
                             type="date"
                             onChange={handleChange}
-                            error={errors.birth}
+                            error={errors.dateNaissance}
                         />
                         <Field
                             label="Téléphone"
@@ -121,7 +196,7 @@ const ClientInfosPage = (props) => {
                             placeHolder="Rue du client"
                             value={client.rue}
                             onChange={handleChange}
-                            error={errors.street}
+                            error={errors.rue}
                         />
                         <Field
                             label="Ville"
@@ -129,7 +204,7 @@ const ClientInfosPage = (props) => {
                             placeHolder="Ville du client"
                             value={client.ville}
                             onChange={handleChange}
-                            error={errors.city}
+                            error={errors.ville}
                         />
                         <Field
                             label="Code Postale"
@@ -137,6 +212,7 @@ const ClientInfosPage = (props) => {
                             placeHolder="Code postale du client"
                             value={client.codePostale}
                             onChange={handleChange}
+                            error={errors.codePostale}
                         />
                         <Field
                             label="Pays"
@@ -144,13 +220,12 @@ const ClientInfosPage = (props) => {
                             placeHolder="Pays du client"
                             value={client.pays}
                             onChange={handleChange}
-                            error={errors.country}
+                            error={errors.pays}
                         />
                         <Field
                             label="Email"
                             name="email"
                             placeHolder="Email du client"
-                            type="email"
                             value={client.email}
                             onChange={handleChange}
                             error={errors.email}
