2beddb84bbe2552d4d5ccab6e1c0879e7e08780f
LeveauArnaud <a.leveau@students.ephec.be>
2020-05-20 19:35:26 +0200

    loading

--- assets/js/pages/ClientsPage.jsx
+++ assets/js/pages/ClientsPage.jsx
@@ -117,7 +117,7 @@ const ClientsPage = (props) => {
 
 
 
-            {clients && itemsPerPage < filteredClients.length &&( <Pagination
+            {loading && itemsPerPage < filteredClients.length &&( <Pagination
                 currentPage={currentPage}
                 itemsPerPage={itemsPerPage}
                 length={filteredClients.length}
