38b538178089fedd5c5cd3df7b23e5cb6614c5e1
LeveauArnaud <a.leveau@students.ephec.be>
2020-05-20 16:04:45 +0200

    up migrations

--- src/Migrations/Version20200416225218.php
+++ src/Migrations/Version20200416225218.php
@@ -22,8 +22,6 @@ final class Version20200416225218 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE invoice DROP FOREIGN KEY FK_906517449395C3F3');
-        $this->addSql('ALTER TABLE customer DROP FOREIGN KEY FK_81398E09A76ED395');
         $this->addSql('CREATE TABLE etat_commande (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE stock (id INT AUTO_INCREMENT NOT NULL, id_monture_id INT NOT NULL, id_magasin_id INT NOT NULL, quantite INT NOT NULL, INDEX IDX_4B36566071805B6B (id_monture_id), INDEX IDX_4B3656608583EA34 (id_magasin_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE monture (id INT AUTO_INCREMENT NOT NULL, marque VARCHAR(255) NOT NULL, model VARCHAR(255) NOT NULL, couleur VARCHAR(255) NOT NULL, taille VARCHAR(255) NOT NULL, prix INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
@@ -44,8 +42,6 @@ final class Version20200416225218 extends AbstractMigration
         $this->addSql('ALTER TABLE commande_verre ADD CONSTRAINT FK_F284081DD5E86FF FOREIGN KEY (etat_id) REFERENCES etat_commande (id)');
         $this->addSql('ALTER TABLE correction ADD CONSTRAINT FK_A29DA1B899DED506 FOREIGN KEY (id_client_id) REFERENCES client (id)');
         $this->addSql('ALTER TABLE correction ADD CONSTRAINT FK_A29DA1B83DD825CA FOREIGN KEY (id_prescripteur_id) REFERENCES prescripteur (id)');
-        $this->addSql('DROP TABLE customer');
-        $this->addSql('DROP TABLE invoice');
         $this->addSql('DROP TABLE user');
     }
 
@@ -64,11 +60,7 @@ final class Version20200416225218 extends AbstractMigration
         $this->addSql('ALTER TABLE commande_monture DROP FOREIGN KEY FK_C8A0E78B99DED506');
         $this->addSql('ALTER TABLE commande_verre DROP FOREIGN KEY FK_F284081D99DED506');
         $this->addSql('ALTER TABLE correction DROP FOREIGN KEY FK_A29DA1B899DED506');
-        $this->addSql('CREATE TABLE customer (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, first_name VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, last_name VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, email VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, INDEX IDX_81398E09A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
-        $this->addSql('CREATE TABLE invoice (id INT AUTO_INCREMENT NOT NULL, customer_id INT NOT NULL, amount DOUBLE PRECISION NOT NULL, sent_at DATETIME NOT NULL, status VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, chrono INT NOT NULL, INDEX IDX_906517449395C3F3 (customer_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, roles JSON NOT NULL, password VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, last_name VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, first_name VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
-        $this->addSql('ALTER TABLE customer ADD CONSTRAINT FK_81398E09A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
-        $this->addSql('ALTER TABLE invoice ADD CONSTRAINT FK_906517449395C3F3 FOREIGN KEY (customer_id) REFERENCES customer (id)');
         $this->addSql('DROP TABLE etat_commande');
         $this->addSql('DROP TABLE stock');
         $this->addSql('DROP TABLE monture');
